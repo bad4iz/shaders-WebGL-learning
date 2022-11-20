24b47abb8e5f158880b9b7a40c03006bf6026b75
bad4iz <developer@bad4iz.ru>
2022-11-20 18:56:56 +0400

    ф

--- mandelbrotSet/main.js
+++ mandelbrotSet/main.js
@@ -15,10 +15,36 @@ const gl = canvas.getContext("webgl");
 const program = await initWebGl('mandelbrotSet.vert', 'mandelbrotSet.frag')
 
 const a_Position = gl.getAttribLocation(program, "a_Position");
+
 const a_PointSize = gl.getAttribLocation(program, "a_PointSize");
-
-
 gl.vertexAttrib1f(a_PointSize, 20.0,);
+
+const u_Resolution = gl.getUniformLocation(program, 'u_Resolution');
+const u_Mouse = gl.getUniformLocation(program, 'u_Mouse');
+const u_Time = gl.getUniformLocation(program, 'u_Time');
+gl.uniform2f(u_Resolution, canvas.width,  canvas.height );
+
+
+// треугольник
+const verticesArr = [
+    -1.0, 1.0,
+    -1.0, -1.0,
+    1., 1.,
+    1.0, -1.0
+];
+const vertices = new Float32Array(verticesArr);
+const n = verticesArr.length;
+const vertexBuffer = gl.createBuffer();
+
+gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+// Assign the buffer object to a_Position variable
+gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+
+// Enable the assignment to a_Position variable
+gl.enableVertexAttribArray(a_Position);
+
+
 
 // Указываем цвет для очистки <canvas>
 gl.clearColor(0.0, 1, 1.0, 1.0);
@@ -26,37 +52,42 @@ gl.clearColor(0.0, 1, 1.0, 1.0);
 
 // Очищаем <canvas>
 gl.clear(gl.COLOR_BUFFER_BIT);
+// Рисуем треугольник
+gl.drawArrays(gl.TRIANGLE_STRIP, 0, n);
 
 
-canvas.addEventListener('click', setPoints(drawPoints))
-
-function setPoints(callbackAfterClick) {
-    const points = [];
-    return function (evt) {
-        const rec = evt.target.getBoundingClientRect();
-
-        const x = 2 * evt.offsetX / rec.width - 1;
-        const y = 1 - 2 * evt.offsetY / rec.height;
-
-        points.push({x, y});
-
-        callbackAfterClick(points)
-    }
-}
+const mouse = {x: 0.1, y: .1};
 
 
-function drawPoints(points) {
+canvas.addEventListener('mousemove',(evt)=>{
+    const evRect = evt.target.getBoundingClientRect();
 
+    const dx = (2 * evt.offsetX) / evRect.width - 1 - mouse.x;
+    const dy = 1 - (2 * evt.offsetY) / evRect.height - mouse.y;
+
+    mouse.x += dx;
+    mouse.y += dy;
+
+})
+
+const tick = function(timer) {
+    // console.log(timer);
     // Очищаем <canvas>
     gl.clear(gl.COLOR_BUFFER_BIT);
+    // Рисуем треугольник
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, n);
+    gl.uniform1f(u_Time, timer/1000 );
+    gl.uniform2f(u_Mouse, mouse.x, mouse.y );
 
-    points.forEach(point => {
-        gl.vertexAttrib3f(a_Position, point.x, point.y, 0, 0)
+    requestAnimationFrame(tick);   // Request that the browser ?calls tick
+};
+tick();
 
-        // Рисуем точку
-        gl.drawArrays(gl.POINTS, 0, 1);
-    });
-}
+
+
+
+
+
 
 /**
  *
